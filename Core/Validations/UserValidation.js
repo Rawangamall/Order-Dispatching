a2f4f5ee36157e22f82a0b1d11409499500bc4f6
Ayman Salah <ayman.1551.salah@gmail.com>
2023-06-08 20:46:27 +0300

    T

--- Core/Validations/UserValidation.js
+++ Core/Validations/UserValidation.js
@@ -1,28 +1,28 @@
-const {body,param}=require("express-validator");
+const { body, param } = require("express-validator");
 
-exports.UserValidPOST =[
-body("firstName").isString().withMessage("fisrt name should string") ,
-body("lastName").isString().withMessage("last name should string") ,
-body("email").isEmail().withMessage("should be valid email form") ,
-body("image").isString().withMessage("image should string") ,
-body("phoneNumber").isNumeric().withMessage("The number should be integer"),
-body("Role").isString().withMessage("Role should string") ,
-body("password").isString().withMessage("password should string") ,
-body("active").isBoolean().withMessage("active should boolean") ,
-]
+exports.UserValidPOST = [
+	body("firstName").isString().withMessage("fisrt name should string"),
+	body("lastName").isString().withMessage("last name should string"),
+	body("email").isEmail().withMessage("should be valid email form"),
+	body("image").isString().withMessage("image should string"),
+	body("phoneNumber").isNumeric().withMessage("The number should be integer"),
+	body("Role").isString().withMessage("Role should string"),
+	body("password").isString().withMessage("password should string"),
+	body("active").isBoolean().withMessage("active should boolean"),
+];
 
-exports.UserValidPUT =[
-param("id").isNumeric().withMessage("id should be integer"),
-body("fisrtName").isString().withMessage("fisrt name should string") ,
-body("lastName").isString().withMessage("last name should string") ,
-body("email").isEmail().withMessage("should be valid email form") ,
-body("image").isString().withMessage("image should string") ,
-body("phoneNumber").isNumeric().withMessage("The number should be integer"),
-body("Role").isString().withMessage("Role should string") ,
-body("password").isString().withMessage("password should string") ,
-body("active").isBoolean().withMessage("active should boolean") ,
-]
+exports.UserValidPUT = [
+	param("id").isNumeric().withMessage("id should be integer"),
+	body("fisrtName").isString().withMessage("fisrt name should string"),
+	body("lastName").isString().withMessage("last name should string"),
+	body("email").isEmail().withMessage("should be valid email form"),
+	body("image").isString().withMessage("image should string"),
+	body("phoneNumber").isNumeric().withMessage("The number should be integer"),
+	body("Role").isString().withMessage("Role should string"),
+	body("password").isString().withMessage("password should string"),
+	body("active").isBoolean().withMessage("active should boolean"),
+];
 
-exports.UserValidId =[
-param("id").isNumeric().withMessage("id should be integer"),
-]
+exports.UserValidId = [
+	param("id").isNumeric().withMessage("id should be integer"),
+];
--- Core/Validations/validateMW.js
+++ Core/Validations/validateMW.js
@@ -1,13 +1,13 @@
-const {validationResult}=require("express-validator")
-module.exports= (request,response,next)=>{
-    let result= validationResult(request);
-    if(result.errors.length!=0)
-    {
-        let errorMsg= result.errors.reduce((current,error)=>current + error.msg+" , ","")
-        let error=new Error(errorMsg);
-        error.status=422; 
-        next(error);
-    }
-    else
-    next();
-}
+const { validationResult } = require("express-validator");
+module.exports = (request, response, next) => {
+	let result = validationResult(request);
+	if (result.errors.length != 0) {
+		let errorMsg = result.errors.reduce(
+			(current, error) => current + error.msg + " , ",
+			""
+		);
+		let error = new Error(errorMsg);
+		error.status = 422;
+		next(error);
+	} else next();
+};
