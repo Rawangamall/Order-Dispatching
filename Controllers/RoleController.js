603b1e5ac622e1d1f9ab879b2e3fb490ab18dcbd
Ayman Salah <ayman.1551.salah@gmail.com>
2023-06-08 20:41:10 +0300

    Formatting

--- Controllers/RoleController.js
+++ Controllers/RoleController.js
@@ -1,173 +1,178 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("./../Models/RoleModel");
 
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/CatchAsync");
 
-const UserSchema=mongoose.model("user");
-const RoleSchema=mongoose.model("role");
-
+const UserSchema = mongoose.model("user");
+const RoleSchema = mongoose.model("role");
 
 exports.getRoles = async (request, response, next) => {
-    try {
-      const roles = await RoleSchema.find();
-      response.json({ roles });
-    } catch (error) {
-      console.log(error);
-      response.status(500).json({ error: 'An error occurred while fetching roles', details: error.message });
-    }
-  };
+	try {
+		const roles = await RoleSchema.find();
+		response.json({ roles });
+	} catch (error) {
+		console.log(error);
+		response
+			.status(500)
+			.json({
+				error: "An error occurred while fetching roles",
+				details: error.message,
+			});
+	}
+};
 
+exports.addRole = async (request, response, next) => {
+	try {
+		const { user_id, name, permissions } = request.body;
 
+		// Check if the role name already exists
+		const existingRole = await RoleSchema.findOne({ name });
+		if (existingRole) {
+			return response
+				.status(400)
+				.json({ error: "Role with the same name already exists" });
+		}
 
-    exports.addRole = async (request, response, next) => {
-        try {
-            const { user_id, name, permissions } = request.body;
-        
-            // Check if the role name already exists
-            const existingRole = await RoleSchema.findOne({ name });
-            if (existingRole) {
-              return response.status(400).json({ error: 'Role with the same name already exists' });
-            }
-        
-            // Create a new role
-            const role = new RoleSchema({
-                user_id,
-                name,
-                permissions: {
-                  statistics: {
-                    viewAll: permissions.statistics.viewAll
-                  },
-                  users: {
-                    viewAll: permissions.users.viewAll,
-                    add: permissions.users.add,
-                    edit: permissions.users.edit,
-                    delete: permissions.users.delete,
-                    activateDeactivate: permissions.users.activateDeactivate
-                  },
-                  orders: {
-                    viewAll: permissions.orders.viewAll,
-                    add: permissions.orders.add,
-                    edit: permissions.orders.edit,
-                    delete: permissions.orders.delete
-                  },
-                  customers: {
-                    viewAll: permissions.customers.viewAll
-                  },
-                  locations: {
-                    view: permissions.locations.view,
-                    add: permissions.locations.add,
-                    edit: permissions.locations.edit
-                  },
-                  drivers: {
-                    viewAll: permissions.drivers.viewAll,
-                    add: permissions.drivers.add,
-                    edit: permissions.drivers.edit,
-                    delete: permissions.drivers.delete
-                  },
-                  roles: {
-                    viewAll: permissions.roles.viewAll,
-                    add: permissions.roles.add,
-                    edit: permissions.roles.edit
-                  }
-                }
-              });
-              
-        
-            // Save the role to the database
-            await role.save();
-        
-            response.json({ message: 'Role created successfully', role });
-          } 
-           catch (error) {
-            console.error(error);
-            response.status(500).json({ error: 'An error occurred while creating the role', details: error.message });
-          }
-          
-    };
+		// Create a new role
+		const role = new RoleSchema({
+			user_id,
+			name,
+			permissions: {
+				statistics: {
+					viewAll: permissions.statistics.viewAll,
+				},
+				users: {
+					viewAll: permissions.users.viewAll,
+					add: permissions.users.add,
+					edit: permissions.users.edit,
+					delete: permissions.users.delete,
+					activateDeactivate: permissions.users.activateDeactivate,
+				},
+				orders: {
+					viewAll: permissions.orders.viewAll,
+					add: permissions.orders.add,
+					edit: permissions.orders.edit,
+					delete: permissions.orders.delete,
+				},
+				customers: {
+					viewAll: permissions.customers.viewAll,
+				},
+				locations: {
+					view: permissions.locations.view,
+					add: permissions.locations.add,
+					edit: permissions.locations.edit,
+				},
+				drivers: {
+					viewAll: permissions.drivers.viewAll,
+					add: permissions.drivers.add,
+					edit: permissions.drivers.edit,
+					delete: permissions.drivers.delete,
+				},
+				roles: {
+					viewAll: permissions.roles.viewAll,
+					add: permissions.roles.add,
+					edit: permissions.roles.edit,
+				},
+			},
+		});
 
-    exports.updateRole = async (request, response, next) => {
-        try {
-          const { id } = request.params;
-          const { permissions } = request.body;
-          const { name } = request.body;
-          
-          // Find the role by its ID
-          const role = await RoleSchema.findById(id);
-          
-          if (!role) {
-            return response.status(404).json({ error: 'Role not found' });
-          }
-          
-          // Update specific fields of the role
-          if(name)
-          {
-             role.name = name;
-          }
-           
-          if (permissions && permissions.statistics) {
-            role.permissions.statistics = permissions.statistics;
-          }
-          
-          if (permissions && permissions.users) {
-            role.permissions.users.viewAll = permissions.users.viewAll;
-            role.permissions.users.add = permissions.users.add;
-            role.permissions.users.edit = permissions.users.edit;
-            role.permissions.users.activateDeactivate = permissions.users.activateDeactivate;
-          }
+		// Save the role to the database
+		await role.save();
 
-            if (permissions && permissions.orders) {
-                role.permissions.orders.viewAll = permissions.orders.viewAll;
-                role.permissions.orders.add = permissions.orders.add;
-                role.permissions.orders.edit = permissions.orders.edit;
-            }
+		response.json({ message: "Role created successfully", role });
+	} catch (error) {
+		console.error(error);
+		response
+			.status(500)
+			.json({
+				error: "An error occurred while creating the role",
+				details: error.message,
+			});
+	}
+};
 
-            if (permissions && permissions.customers) {
-                role.permissions.customers.viewAll = permissions.customers.viewAll;
-            }
+exports.updateRole = async (request, response, next) => {
+	try {
+		const { id } = request.params;
+		const { permissions } = request.body;
+		const { name } = request.body;
 
-            if (permissions && permissions.locations) {
-                role.permissions.locations.view = permissions.locations.view;
-                role.permissions.locations.add = permissions.locations.add;
-                role.permissions.locations.edit = permissions.locations.edit;
-            }
+		// Find the role by its ID
+		const role = await RoleSchema.findById(id);
 
-            if (permissions && permissions.drivers) {
-                role.permissions.drivers.viewAll = permissions.drivers.viewAll;
-                role.permissions.drivers.add = permissions.drivers.add;
-                role.permissions.drivers.edit = permissions.drivers.edit;
+		if (!role) {
+			return response.status(404).json({ error: "Role not found" });
+		}
 
-            }
+		// Update specific fields of the role
+		if (name) {
+			role.name = name;
+		}
 
-            if (permissions && permissions.roles) {
-                role.permissions.roles.viewAll = permissions.roles.viewAll;
-                role.permissions.roles.add = permissions.roles.add;
-                role.permissions.roles.edit = permissions.roles.edit;
-            }
-    
-          // Update other fields in a similar way
-          
-          // Save the updated role
-          await role.save();
-          
-          response.json({ message: 'Role updated successfully', role });
-        } catch (error) {
-          console.error(error);
-          response.status(500).json({ error: 'An error occurred while updating the role', details: error.message });
-        }
-      };
-      
+		if (permissions && permissions.statistics) {
+			role.permissions.statistics = permissions.statistics;
+		}
 
-    
-      
-exports.getRoleById=(request,response,next)=>{
-    RoleSchema.findById(request.params.id)
-                    .then((data)=>{
-                        response.status(200).json(data);
-                    })
-                    .catch(error=>{
-                        next(error);
-                    })
-}
+		if (permissions && permissions.users) {
+			role.permissions.users.viewAll = permissions.users.viewAll;
+			role.permissions.users.add = permissions.users.add;
+			role.permissions.users.edit = permissions.users.edit;
+			role.permissions.users.activateDeactivate =
+				permissions.users.activateDeactivate;
+		}
 
+		if (permissions && permissions.orders) {
+			role.permissions.orders.viewAll = permissions.orders.viewAll;
+			role.permissions.orders.add = permissions.orders.add;
+			role.permissions.orders.edit = permissions.orders.edit;
+		}
 
+		if (permissions && permissions.customers) {
+			role.permissions.customers.viewAll = permissions.customers.viewAll;
+		}
+
+		if (permissions && permissions.locations) {
+			role.permissions.locations.view = permissions.locations.view;
+			role.permissions.locations.add = permissions.locations.add;
+			role.permissions.locations.edit = permissions.locations.edit;
+		}
+
+		if (permissions && permissions.drivers) {
+			role.permissions.drivers.viewAll = permissions.drivers.viewAll;
+			role.permissions.drivers.add = permissions.drivers.add;
+			role.permissions.drivers.edit = permissions.drivers.edit;
+		}
+
+		if (permissions && permissions.roles) {
+			role.permissions.roles.viewAll = permissions.roles.viewAll;
+			role.permissions.roles.add = permissions.roles.add;
+			role.permissions.roles.edit = permissions.roles.edit;
+		}
+
+		// Update other fields in a similar way
+
+		// Save the updated role
+		await role.save();
+
+		response.json({ message: "Role updated successfully", role });
+	} catch (error) {
+		console.error(error);
+		response
+			.status(500)
+			.json({
+				error: "An error occurred while updating the role",
+				details: error.message,
+			});
+	}
+};
+
+exports.getRoleById = (request, response, next) => {
+	RoleSchema.findById(request.params.id)
+		.then((data) => {
+			response.status(200).json(data);
+		})
+		.catch((error) => {
+			next(error);
+		});
+};
