cd1e9b50289e3f7aef6900aa4f69768a6a66458c
Ayman Salah <ayman.1551.salah@gmail.com>
2023-06-15 01:46:14 +0300

    Customers Basic ROute

---
+++ Controllers/CustomerController.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("./../Models/OrderModel");
+
+const OrderSchema = mongoose.model("order");
+
+exports.getAllCustomers = (request, response, next) => {
+	OrderSchema.find({} , 'CustomerID CustomerName CustomerEmail -_id')
+		.then((data) => {
+			response.status(200).json(data);
+		})
+		.catch((error) => {
+			next(error);
+		});
+};
--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const loginRoute = require("./Routes/loginRoute");
 const OrderRoute = require("./Routes/OrderRoute");
 const locationRoute = require("./Routes/LocationRoute");
 const DispatchRoute = require("./Routes/DispatchRoute");
-const refresh = require("./github_refresh/refresh")
-const client = require("./client")
+const CustomersRoute = require("./Routes/CustomerRoute");
+const refresh = require("./github_refresh/refresh");
+const client = require("./client");
 const socketIO = require("socket.io");
-const { io , server , app  } = require("./utils/socket");
+const { io, server, app } = require("./utils/socket");
 //server
 
 let port = process.env.PORT || 8080;
@@ -23,23 +24,23 @@ let port = process.env.PORT || 8080;
 mongoose.set("strictQuery", true); //warning
 
 mongoose
-  .connect(
-    "mongodb+srv://OrderDispatching:iti@cluster0.eesrbrh.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("DB connected");
-    server.listen(port, () => {
-      console.log("server is listenng.....", port);
-    });
-  })
-  .catch((error) => {
-    console.log("Db Problem " + error);
-  });
+	.connect(
+		"mongodb+srv://OrderDispatching:iti@cluster0.eesrbrh.mongodb.net/?retryWrites=true&w=majority"
+	)
+	.then(() => {
+		console.log("DB connected");
+		server.listen(port, () => {
+			console.log("server is listenng.....", port);
+		});
+	})
+	.catch((error) => {
+		console.log("Db Problem " + error);
+	});
 
 app.use(
-  cors({
-    origin: "*",
-  })
+	cors({
+		origin: "*",
+	})
 );
 //body parse
 app.use(express.json());
@@ -56,21 +57,23 @@ app.use(OrderRoute);
 app.use(DriverRoute);
 app.use(locationRoute);
 app.use(DispatchRoute);
+app.use(CustomersRoute);
+
 
 //Not Found Middleware
 app.use((request, response, next) => {
-  response
-    .status(404)
-    .json({ message: `${request.originalUrl} not found on this server!` });
+	response
+		.status(404)
+		.json({ message: `${request.originalUrl} not found on this server!` });
 });
 
 //Global error handeling Middleware
 app.use((error, request, response, next) => {
-  if (error.statusCode && error.statusCode !== 500) {
-    // the specific status code from the AppError
-    response.status(error.statusCode).json({ message: error.message });
-  } else {
-    //the default 500 status code handling
-    response.status(500).json({ message: error + "" });
-  }
+	if (error.statusCode && error.statusCode !== 500) {
+		// the specific status code from the AppError
+		response.status(error.statusCode).json({ message: error.message });
+	} else {
+		//the default 500 status code handling
+		response.status(500).json({ message: error + "" });
+	}
 });
